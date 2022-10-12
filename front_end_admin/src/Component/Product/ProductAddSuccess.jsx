b26242e1e5882805871e610dfd623c5c5ced8c9f
HoangBa99 <phanbahoang10@gmail.com>
2022-10-12 13:29:42 +0700

    fix: fix mess when add product success

--- front_end_admin/src/Component/Product/ProductAddSuccess.jsx
+++ front_end_admin/src/Component/Product/ProductAddSuccess.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const ProductAddSuccess = () => {
     return (
         <>
-            <Typography variant='h6' sx={{ color: 'green', mt: '20px', mb: '20px', textAlign: 'center' }}>Add product success</Typography>
+            <Typography variant='h6' sx={{ color: 'green', mt: '20px', mb: '20px', textAlign: 'center' }}>Add product success. Please go to page last for more detail</Typography>
         </>
     )
 }
